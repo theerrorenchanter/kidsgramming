4e181490086b34626756b909207a161f44600db2
dicarlomtz <dicarlomtz@gmail.com>
2022-09-25 15:04:15 -0600

    Frontend improvments

--- kidsgramming-fe/src/home/components/HomeWelcome.jsx
+++ kidsgramming-fe/src/home/components/HomeWelcome.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { Typography } from '@mui/material'
+
+const subtitleStyles = {
+  background: '-webkit-linear-gradient(45deg, #7722e7 30%, #FF8E53 90%)',
+  WebkitBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent',
+  textTransform: 'uppercase',
+  fontFamily: 'monospace',
+  fontSize: 50,
+  letterSpacing: 3,
+  fontWeight: 700,
+  color: 'primary.main'
+}
+
+const titleStyles = { textTransform: 'uppercase', fontFamily: 'monospace', fontWeight: 'bold', fontSize: 25, letterSpacing: 5 }
 
 export const HomeWelcome = () => {
   return (
-    <div>HomeWelcome</div>
+    <>
+      <Typography component='h1' sx={titleStyles}>Kidsgramming</Typography>
+      <Typography mt={1} component='h2' sx={ subtitleStyles }>Your kids are our future</Typography>
+      <Typography mt={ 2 } variant='body1' sx={{ fontSize: 20, fontStyle: 'italic', color: 'grey.600' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Typography>
+    </>
   )
 }
--- kidsgramming-fe/src/home/pages/HomePage.jsx
+++ kidsgramming-fe/src/home/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { HomeLayout } from '../layout/HomeLayout'
 import { Navbar } from '../../navigation/views/Navbar'
 import { HomeItemLayout } from '../layout/HomeItemLayout'
 import { Paper } from '@mui/material'
+import { HomeWelcome } from '../components/HomeWelcome'
 
 export const HomePage = () => {
   return (
@@ -11,7 +12,7 @@ export const HomePage = () => {
       <Navbar />
       <HomeLayout>
         <HomeItemLayout component={ Paper } elevation={ 1 } sx={{ width: '75%', height: '75%' }}>
-          asdas
+          <HomeWelcome />
         </HomeItemLayout>
         <Image src='/home/img/home.png' sx={{ width: '50%' }} />
       </HomeLayout>
