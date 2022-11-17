93d24b89bd5f7937c8ed89e991bc2030faa010b2
Brandon Rodriguez Rodriguez <brandonrod@hotmail.com>
2022-11-16 22:26:50 -0600

    se agregan imagens nuevas y se cambian las de profesor y estudiante

--- kidsgramming-fe/src/sponsors/views/RegisterStudentForm.jsx
+++ kidsgramming-fe/src/sponsors/views/RegisterStudentForm.jsx
@@ -10,7 +10,7 @@ export const RegisterStudentForm = ({ authForm }) => {
   const { touched, errors, values, handleChange, isSubmitting } = authForm
 
   return (
-    <AuthFormLayout title='Register Student' imageSource='/auth/img/register.png'>
+    <AuthFormLayout title='Register Student' imageSource='/auth/img/addstudent.png'>
       <form onSubmit={ authForm.handleSubmit }>
         <Grid container >
           <Grid item xs={12} sx={{ mt: 2 }}>
